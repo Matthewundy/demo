0698d77a447856a566674a30eb1d6aa1969732f1
Matthew Undy <matthewundy@googlemail.com>
2016-07-12 16:22:31 +0100

    Update

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
         name = "MyServlet",
-        urlPatterns = {"/"}
+        urlPatterns = {"/login"}
     )
 public class HelloServlet extends HttpServlet {
 
