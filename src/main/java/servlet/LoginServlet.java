4c9bf26647b41e7b8ddcf210f5b21a25465227b0
Matthew Undy <matthewundy@googlemail.com>
2016-07-12 22:05:33 +0100

    Update

---
+++ src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,46 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.okta.sdk.clients.SessionApiClient;
+import com.okta.sdk.clients.UserApiClient;
+import com.okta.sdk.exceptions.ApiException;
+import com.okta.sdk.framework.ApiClientConfiguration;
+import com.okta.sdk.models.sessions.Session;
+import com.okta.sdkdemo.okta.OktaApiController;
+
+@WebServlet(
+        name = "LoginServlet",
+        urlPatterns = {"/login"}
+    )
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+    	req.getRequestDispatcher("login_proc.jsp").forward(req, resp);
+		return;
+    }
+    
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+		
+                String username = req.getParameter("UserName"); 
+		String password = req.getParameter("Password");
+
+		try{
+			Session mySession = sessionClient.createSessionWithCredentials(username, password);
+		} catch (ApiException e) {
+		}
+		res.sendRedirect("https://euromonitorpoc.oktapreview.com/app/euromonitorpoc_euromonitorpassport_1/exk6mqdgvsyvhEIE20h7/sso/saml");
+		return;
+    }
+
+}
