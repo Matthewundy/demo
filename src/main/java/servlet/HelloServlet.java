d885eebc702d76e882398c5e2a0d3abd674b81b0
Matthew Undy <matthewundy@googlemail.com>
2016-07-12 16:10:42 +0100

    Update

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-    	req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+    	req.getRequestDispatcher("login.jsp").forward(req, resp);
 		return;
     }
 
