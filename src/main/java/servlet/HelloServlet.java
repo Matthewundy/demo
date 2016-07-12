a912003fd5592ef12c8cc91f2a47dadf0dc8abf1
Matthew Undy <matthewundy@googlemail.com>
2016-07-12 17:53:46 +0100

    Update

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -8,6 +8,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 @WebServlet(
         name = "MyServlet",
@@ -31,7 +43,7 @@ public class HelloServlet extends HttpServlet {
 			Base64.Decoder decoder = Base64.getDecoder();
 			byte[] base64DecodedResponse = decoder.decode(responseMessage);
 			String s = new String(base64DecodedResponse);
-			request.setAttribute("saml",s);
+			req.setAttribute("saml",s);
 			ByteArrayInputStream input = new ByteArrayInputStream(base64DecodedResponse);
 		
 			try {
@@ -41,7 +53,7 @@ public class HelloServlet extends HttpServlet {
 				NodeList nList = document.getElementsByTagName("saml2:Subject");
 				Node samlSubject = nList.item(0);
 				
-				request.setAttribute("username",samlSubject.getTextContent());
+				req.setAttribute("username",samlSubject.getTextContent());
 				
 			} catch (ParserConfigurationException e) {
 				
